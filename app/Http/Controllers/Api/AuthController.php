31fb50202c2c3c5e4c7def4fcd0b5b2852e878ea
Boloyyu90 <balaputra20@gmail.com>
2023-11-14 09:07:59 +0700

    first_commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request)
+    {
+        $registrationData = $request->all();
+
+        $validate = Validator::make($registrationData, [
+            'name' => 'required|max:60',
+            'email' => 'required|email:rfc,dns|unique:users',
+            'password' => 'required',
+            'no_telp' => 'required',
+        ]);
+        if ($validate->fails()) {
+            return response(['messaage' => $validate->errors()], 400);
+        }
+
+        $registrationData['status'] = 0;
+        $registrationData['password'] = bcrypt($request->password);
+
+        $user = User::create($registrationData);
+
+        return response([
+            'message' => 'Register Success',
+            'user' => $user
+        ], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $loginData = $request->all();
+        $validate = Validator::make($loginData, [
+            'email' => 'required|email:rfc,dns',
+            'password' => 'required',
+        ]);
+
+        if ($validate->fails()) {
+            return response(['message' => $validate->errors()], 400);
+        }
+
+        if (!Auth::attempt($loginData)) {
+            return response(['message' => 'Invalid Credential'], 401);
+        }
+
+        /** @var \App\Models\User $user **/
+        $user = Auth::user();
+        $token = $user->createToken('Authetication Token')->accessToken;
+
+        return response([
+            'message' => 'Autheticated',
+            'user' => $user,
+            'token_type' => 'Bearer',
+            'access_token' => $token
+        ]);
+    }
+}
